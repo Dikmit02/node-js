de5b2f8ac88874bcb27eddd8040bbd1f6c048d5b
diksha mittal <dikshamittal21297@gmail.com>
2019-07-16 16:37:34 +0530

    chalk module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
-const validator=require('validator')
+const chalk=require('chalk')
 const getNotes=require('./notes.js')
 const msg=getNotes()
-console.log(msg)
+console.log(chalk.green(msg))
 
-console.log(validator.isEmail('diksha@example.com'));
+// console.log(validator.isEmail('diksha@example.com'));
 
 // const string=require('./notes.js')
 // console.log(string)
