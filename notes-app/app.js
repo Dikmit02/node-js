eecee43f23d5aeb2b4efa050933b5de633ea5285
diksha mittal <dikshamittal21297@gmail.com>
2019-07-16 19:55:57 +0530

    arguments parsing by args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,23 +1,49 @@
-const chalk=require('chalk')
-const getNotes=require('./notes.js')
-const command=process.argv[2]
-console.log(command);
-//node app.js remove add
-console.log(process.argv);//[
-//     'C:\\Program Files\\nodejs\\node.exe',
-//     'C:\\Users\\diksh\\Desktop\\node-js\\notes-app\\app.js',
-//     'remove',
-//     'add'
-//   ]
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-if(command==='add'){
-    console.log('Adding notes');
-}
-else if(command==='remove'){
-    console.log('Deleting notes');
-}
-//node app.js remove output is Deleting notes
+// Customize yargs version
+yargs.version('1.1.0')
 
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title:{
+            describe: 'Note title'
+        }
+    },
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
 
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
 
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
 
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)
