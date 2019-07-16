bb539c6067dcf58f047f2ffb768690ff55a36724
diksha mittal <dikshamittal21297@gmail.com>
2019-07-16 15:37:37 +0530

    npm module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,14 @@
-const add=require('./utils.js')
-sum=add(2,-5)
-console.log(sum);
+const validator=require('validator')
+const getNotes=require('./notes.js')
+const msg=getNotes()
+console.log(msg)
+
+console.log(validator.isEmail('diksha@example.com'));
+
+// const string=require('./notes.js')
+// console.log(string)
+//[Function: getNotes] output
+
+
+
 
