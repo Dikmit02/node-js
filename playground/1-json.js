aa9f9ed5a4ff9d8712d511b2c92309890588edcf
diksha mittal <dikshamittal21297@gmail.com>
2019-07-17 00:44:58 +0530

    changing the data of json

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,23 +1,9 @@
  const fs=require('fs')
-// const book={
-//     title:"wings of fire",
-//     autho:"Dr. A P J Abdul Kalam"
-
-// }
-// const bookJSOn=JSON.stringify(book);
-// //console.log(bookJSOn);//{"title":"wings of fire","autho":"Dr. A P J Abdul Kalam"}
-
-// // writing the json object to another file
-// fs.writeFileSync('1-json.json',bookJSOn)
-
-
-
-
-
-
 const dataBuffer=fs.readFileSync('1-json.json')
 const dataJSON=dataBuffer.toString()
 const data=JSON.parse(dataJSON)
-console.log(data);
-console.log(data.title);
-console.log(data.author);
+
+data.name="diksha"
+data.age=4
+const dataJSON=JSON.stringify/9(data);
+fs.writeFileSync('i-json.json',dataJSON)
