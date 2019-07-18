8531c0dfd04c2e442ab70bf35dc4a9d9615545b4
diksha mittal <dikshamittal21297@gmail.com>
2019-07-18 15:29:47 +0530

    callback absttraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,15 @@
- const request=require('request')
-// const url='https://api.darksky.net/forecast/1e4f60a6b5a12f81c5ec15e25f4a0c11/37.8267'
-// request({url:url,json:true},(error,response)=>{
-//    if(error){
-//       console.log('Unable to connect to weather service!!!');
-//    }
-//    else if(response.body.error){
-//       console.log('unable to find the loaction');
-//    }
-//    else{
-//        console.log(response.body.daily.data[0].summary+' it is currently '+response.body.currently.temperature+' deegree out.there is '+response.body.currently.precipProbability+'% chance of rain.');
 
-//    }
-    
+ const geocode=require('./utils/geocode')
+ const forecast=require('./utils/forecast')
+
+
+//geocode('delhi',(error,data)=>{
+// console.log('Error',error);
+// console.log('Data',data);
 // })
-const url='https://api.mapbox.com/geocoding/v5/mapbox.places/New%20Delhi.json?access_token=pk.eyJ1IjoiZGlrc2hhbWl0dGFsIiwiYSI6ImNqeTdvZGp2OTAxbWMzbXFsbndnN29yOXAifQ.Sn5xZqzPGFqKEhM3tc1S0A'
-request({url:url,json:true},(error,response)=>{
-   
 
-if(error){
-   console.log('Unable to connect to weather service!!!');
-}
-else if(response.body.features.length==0){
-   //const url='https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoiZGlrc2hhbWl0dGFsIiwiYSI6ImNqeTdvZGp2OTAxbWMzbXFsbndnN29yOXAifQ.Sn5xZqzPGFqKEhM3tc1S0A'
-
-   console.log('unable to find the loaction');
-}
-else{
-   console.log(response.body.features[0].center[0]+' is longitude'+response.body.features[0].center[1]+' is the latitude');
-   
-}
+forecast(37.8267,-122.4233,(error,data)=>{
+   console.log('Error',error);
+   console.log('Data',data);
 })
+
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/1e4f60a6b5a12f81c5ec15e25f4a0c11/' + latitude + ',' + longitude
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+//const url='https://api.darksky.net/forecast/1e4f60a6b5a12f81c5ec15e25f4a0c11/37.8267,-122.4233'
+// request({url:url,json:true},(error,response)=>{
+//    if(error){
+//       console.log('Unable to connect to weather service!!!');
+//    }
+//    else if(response.body.error){
+//       console.log('unable to find the loaction');
+//    }
+//    else{
+//        console.log(response.body.daily.data[0].summary+' it is currently '+response.body.currently.temperature+' deegree out.there is '+response.body.currently.precipProbability+'% chance of rain.');
+
+//    }
+    
+// })
+module.exports=forecast
